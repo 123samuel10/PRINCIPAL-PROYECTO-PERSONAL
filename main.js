e562ce71815ed7b1d366ed48c7fb236839a6c6a7
Samuel <samuelsalcedoramirez@gmail.com>
2022-07-13 22:52:37 -0500

    estudiante 5

--- main.js
+++ main.js
@@ -175,6 +175,104 @@ function enviarEstudinate4(){
     let nombreDeEstudiante4=document.getElementById("nombreCuatro").value;
     alert(`El estudiante es ${nombreDeEstudiante4}`)
 }
+function enviarNotasE4(){
+    let cantidadDeNotaUno4=document.getElementById("notaNumeroEstudinate4.1").value;
+    let cantidadDeNotaDos4=document.getElementById("notaNumeroEstudinate4.2").value;
+    let cantidadDeNotaTres4=document.getElementById("notaNumeroEstudinate4.3").value;
+    let cantidadDeNotaCuatro4=document.getElementById("notaNumeroEstudinate4.4").value;
+  
+
+    cantidadDeNotaUno4=parseInt(cantidadDeNotaUno4);
+    cantidadDeNotaDos4=parseInt(cantidadDeNotaDos4);
+    cantidadDeNotaTres4=parseInt(cantidadDeNotaTres4);
+    cantidadDeNotaCuatro4=parseInt(cantidadDeNotaCuatro4);
+    guardareNotasCuartoEstudiante.push(cantidadDeNotaUno4,cantidadDeNotaDos4,cantidadDeNotaTres4,cantidadDeNotaCuatro4)
+
+}
+function notaMayorE4(){
+    
+        let notaMayorEstudianteCuatro=0;
+        guardareNotasCuartoEstudiante.forEach(element=>{
+            if(element>notaMayorEstudianteCuatro){
+                notaMayorEstudianteCuatro=element;
+            }
+        })
+    alert(`La nota mayor obtenida por el estudiante es ${notaMayorEstudianteCuatro}`)
+}
+
+function notaMenorE4(){
+  let notaMenorCuatro4=1000;
+  for(let i=0; i<guardareNotasCuartoEstudiante.length;i++){
+    if(guardareNotasCuartoEstudiante[i]<notaMenorCuatro4){
+        notaMenorCuatro4=guardareNotasCuartoEstudiante[i]
+    }
+  }
+  alert(notaMenorCuatro4)
+
+}
+function promedioEstudianteCuatro(){
+    let promedio4=0;
+    let sumaEst4=0;
+    for(let i=0; i<guardareNotasCuartoEstudiante.length;i++){
+        sumaEst4=sumaEst4+guardareNotasCuartoEstudiante[i];
+        promedio4=sumaEst4/guardareNotasCuartoEstudiante.length;
+      
+    }
+    alert("El promedio obtenido por el estudiante es:"+  promedio4)
+}
+// -----------------------------------------------------------------------------------------------
+//quinto estudiante
+let guardareNotasQuintoEstudiante=[];
+function enviarEstudiante5(){
+    let estudiante5=document.getElementById("nombreQuinto").value;
+    alert("el estudiante es "+" "+estudiante5)
+}
+function enviarNotasE5(){
+    let cantidadDeNota1=document.getElementById("notaNumeroEstudinate5.1").value;
+    let cantidadDeNota2=document.getElementById("notaNumeroEstudinate5.2").value;
+    let cantidadDeNota3=document.getElementById("notaNumeroEstudinate5.3").value;
+    let cantidadDeNota4=document.getElementById("notaNumeroEstudinate5.4").value;
+
+    cantidadDeNota1=parseInt(cantidadDeNota1);
+    cantidadDeNota2=parseInt(cantidadDeNota2);
+    cantidadDeNota3=parseInt(cantidadDeNota3);
+    cantidadDeNota4=parseInt(cantidadDeNota4);
+    guardareNotasQuintoEstudiante.push(cantidadDeNota1,cantidadDeNota2,cantidadDeNota3,cantidadDeNota4);
+
+}
+
+function notaMayorE5(){
+    let numero=0;
+   
+        guardareNotasQuintoEstudiante.forEach(element=>{
+            if(element>numero){
+                numero=element;
+            }
+        })
+        alert(`la nota mayor obtenida es ${numero}`)
+
+    }
+function notaMenorE5(){
+    let numerob5=1000;
+    for(let i=0;i<guardareNotasQuintoEstudiante.length;i++){
+        if(guardareNotasQuintoEstudiante[i]<numerob5){
+            numerob5=guardareNotasQuintoEstudiante[i];
+        }
+    }
+    alert(`la nota menor obtenida es ${numerob5}`)
+}
+function promedioEstudianteQuinto(){
+    let promedio=0;
+    let suma=0;
+     for(let i=0; i<guardareNotasQuintoEstudiante.length;i++){
+        suma+=guardareNotasQuintoEstudiante[i]
+        promedio=suma/guardareNotasQuintoEstudiante.length;   
+     }
+     alert(`el promedio del estudiante es ${promedio}`)
+   
+}
+
+
 
 
 
@@ -190,22 +288,39 @@ let promedio1=0;
 
 for(let i of NguardarNota){
     suma1+=i;
-    promedio1=suma1/NguardarNota.length;
+   promedio1=suma1/NguardarNota.length;
 }
+// ----------------------------------
 let suma2=0;
 let promedio2=0;
 for(let i of guardarNotasSegundoEstudiante){
     suma2+=i
     promedio2=suma2/guardarNotasSegundoEstudiante.length
 }
+// ------------------------------------------------------
 let suma3=0;
 let promedio3=0;
 for(let i of guardarNotasTercerEstudiante){
     suma3+=i;
     promedio3=suma3/guardarNotasTercerEstudiante.length;
+
+}
+// ------------------------------------------------------------
+let suma4=0;
+let promedio4=0;
+for(let i of guardareNotasCuartoEstudiante){
+    suma4+=i;
+    promedio4=suma4/guardareNotasCuartoEstudiante.length;
+}
+// -------------------------------------------
+let suma5=0;
+let promedio5=0;
+for(let i of guardareNotasQuintoEstudiante){
+    suma5+=i
+    promedio5=suma5/guardareNotasQuintoEstudiante.length;
 }
 
-alert(`el promedio es`+(promedio1+promedio2+promedio3))
+alert(`el promedio es`+` `+(promedio1+promedio2+promedio3+promedio4+promedio5))
 }
 
 
